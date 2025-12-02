83708a1580fcb1115cef49e4e8d566b22fa8f812
Lukas Manuel Graf <graflukas218@gmail.com>
2025-12-02 15:34:48 +0100

    feat(components): add components directory to support writing react components for webflow

---
+++ src/components/main.dev.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Badge } from "./badge";
+
+// Import any component inside your repo:
+
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Components Playground</h1>
+      <Badge text="Hello Motherfuckers" variant="Light"></Badge>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
